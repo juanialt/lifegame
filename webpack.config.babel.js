4cf7bf93bf15d6b2392e9099356d0801b81cf9ae
Juan Ignacio Alterio <juanialt@gmail.com>
2017-03-20 18:04:13 -0300

    webpack dev server setup

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -8,6 +8,12 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
 
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
+    }
+
     devtool: 'eval',
 
     stats: {
